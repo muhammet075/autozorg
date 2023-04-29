1daa30cd704228f2a90bf2aa7bf562dab5869786
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-04-29 02:12:07 +0200

    working on onderhoud

--- src/components/onderhoudprogress.js
+++ src/components/onderhoudprogress.js
@@ -23,7 +23,7 @@ let volgnummer;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+      alert("Geolocation is not supported by this browser.");
     };
 
     function showPosition(position) {
