1b943afb633d69ad69dada3c92be065532d36905
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-04-29 03:54:50 +0200

    adding PWA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import Header from "../components/header";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.innerWidth <= 480) {
+        window.scrollTo(0, 1);
+      }
+    };
+    window.addEventListener("load", handler);
+    window.addEventListener("resize", handler);
+    return () => {
+      window.removeEventListener("load", handler);
+      window.removeEventListener("resize", handler);
+    };
+  }, []);
+
   return (
     <div id='app'>
       <Header />
