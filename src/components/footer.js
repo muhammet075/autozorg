55f05f2e12617ce1694653ffeffedcc2a4cbf36d
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-05-03 05:35:54 +0200

    fixing afspraken

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styles from "../styles/footer.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -7,6 +8,48 @@ import PechhulpIco from "../assets/icons/pechhulp.svg";
 import CalendarIco from "../assets/icons/calendar.svg";
 
 function Footer() {
+
+  useEffect(() => {
+  
+const service = localStorage.getItem("serviceafspraak");
+const apk = localStorage.getItem("apkafspraak");
+const onderhoud = localStorage.getItem("onderhoudafspraak");
+
+if (service && apk && onderhoud) {
+  console.log("Er zijn afspraken gemaakt voor service, APK en onderhoud.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "3";
+} else if (service && apk) {
+  console.log("Er zijn afspraken gemaakt voor service en APK.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "2";
+} else if (service && onderhoud) {
+  console.log("Er zijn afspraken gemaakt voor service en onderhoud.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "2";
+} else if (apk && onderhoud) {
+  console.log("Er zijn afspraken gemaakt voor APK en onderhoud.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "2";
+} else if (service) {
+  console.log("Er is een afspraak gemaakt voor alleen service.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "1";
+} else if (apk) {
+  console.log("Er is een afspraak gemaakt voor alleen APK.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "1";
+} else if (onderhoud) {
+  console.log("Er is een afspraak gemaakt voor alleen onderhoud.");
+  document.querySelector(".aantalafspraken").classList.remove("displaynone");
+  document.querySelector(".aantalafspraken").innerHTML = "1";
+} else {
+  console.log("Er zijn geen afspraken gevonden.");
+  document.querySelector(".aantalafspraken").innerHTML = "0";
+  document.querySelector(".aantalafspraken").classList.add("displaynone")
+}
+  
+  }, []);
 
   return (
     <footer className={styles.footer}>
@@ -28,6 +71,7 @@ function Footer() {
         <div>
           <Image src={CalendarIco} alt='Afspraken Icoon' />
           <p>Afspraken</p>
+          <span className='aantalafspraken'></span>
         </div>
       </Link>
 
